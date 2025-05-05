91563350f3e559456d0b6cc7d3833cd7bd655b71
Pelle Stenild Coltau <psc@dnmh.dk>
2025-05-05 14:52:29 +0200

    Added unit test

--- Tests/IpRestriction/IpRestrictionAuditorTests.cs
+++ Tests/IpRestriction/IpRestrictionAuditorTests.cs
@@ -5,18 +5,13 @@ namespace DNMH.Security.IpRestriction.Tests;
 
 public class IpRestrictionAuditorTests
 {
-    private static readonly IPAddress AllowedIpv4 = IPAddress.Parse("10.0.0.1");
-    private static readonly IPAddress DeniedIpv6 = IPAddress.Parse("2001:db8::223:14ff:feaa:6680");
-    private static readonly IPAddress DeniedIpv4 = IPAddress.Parse("203.0.113.42");
-    private static readonly IPAddress UnlistedIpv4 = IPAddress.Parse("192.168.99.99");
-
     [Fact]
     public void AllowsIpv4_WhenNoRules()
     {
         var options = new IpRestrictionOptions();
 
         var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
-        auditor.IsIpAllowed(null, AllowedIpv4).ShouldBeTrue();
+        auditor.IsIpAllowed(null, IPAddress.Parse("10.0.0.1")).ShouldBeTrue();
     }
 
     [Fact]
@@ -31,8 +26,8 @@ public class IpRestrictionAuditorTests
         };
 
         var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
-        auditor.IsIpAllowed(null, DeniedIpv4).ShouldBeFalse();
-        auditor.IsIpAllowed(null, AllowedIpv4).ShouldBeTrue();
+        auditor.IsIpAllowed(null, IPAddress.Parse("203.0.113.42")).ShouldBeFalse();
+        auditor.IsIpAllowed(null, IPAddress.Parse("10.0.0.1")).ShouldBeTrue();
     }
 
     [Fact]
@@ -47,7 +42,7 @@ public class IpRestrictionAuditorTests
         };
 
         var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
-        auditor.IsIpAllowed(null, DeniedIpv6).ShouldBeFalse();
+        auditor.IsIpAllowed(null, IPAddress.Parse("2001:db8::223:14ff:feaa:6680")).ShouldBeFalse();
     }
 
     [Fact]
@@ -65,8 +60,8 @@ public class IpRestrictionAuditorTests
         };
 
         var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
-        auditor.IsIpAllowed("AdminPanel", DeniedIpv4).ShouldBeFalse();
-        auditor.IsIpAllowed("AdminPanel", AllowedIpv4).ShouldBeTrue();
+        auditor.IsIpAllowed("AdminPanel", IPAddress.Parse("203.0.113.42")).ShouldBeFalse();
+        auditor.IsIpAllowed("AdminPanel", IPAddress.Parse("10.0.0.1")).ShouldBeTrue();
     }
 
     [Fact]
@@ -84,8 +79,8 @@ public class IpRestrictionAuditorTests
         };
 
         var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
-        auditor.IsIpAllowed("AdminPanel", AllowedIpv4).ShouldBeTrue();
-        auditor.IsIpAllowed("AdminPanel", DeniedIpv4).ShouldBeFalse();
+        auditor.IsIpAllowed("AdminPanel", IPAddress.Parse("10.0.0.1")).ShouldBeTrue();
+        auditor.IsIpAllowed("AdminPanel", IPAddress.Parse("203.0.113.42")).ShouldBeFalse();
     }
 
     [Fact]
@@ -103,7 +98,32 @@ public class IpRestrictionAuditorTests
         };
 
         var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
-        auditor.IsIpAllowed("AdminPanel", UnlistedIpv4).ShouldBeFalse();
+        auditor.IsIpAllowed("AdminPanel", IPAddress.Parse("192.168.99.99")).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void AllowsIpv4_WhenProfileBroadensRange()
+    {
+        var options = new IpRestrictionOptions
+        {
+            Global = new()
+            {
+                Allow = ["10.0.0.0/24"]
+            },
+            Profiles = new()
+            {
+                ["AdminPanel"] = new()
+                {
+                    Allow = ["10.0.0.0/16"]
+                }
+            }
+        };
+
+        var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
+        auditor.IsIpAllowed(null, IPAddress.Parse("10.0.0.0")).ShouldBeTrue();
+        auditor.IsIpAllowed(null, IPAddress.Parse("10.0.1.0")).ShouldBeFalse();
+        auditor.IsIpAllowed("AdminPanel", IPAddress.Parse("10.0.1.0")).ShouldBeTrue();
+        auditor.IsIpAllowed("AdminPanel", IPAddress.Parse("10.1.0.0")).ShouldBeFalse();
     }
 
     [Fact]
@@ -129,15 +149,15 @@ public class IpRestrictionAuditorTests
         var auditor = new IpRestrictionAuditor(OptionsMonitor.Create(options), NullLoggerFactory.Instance);
 
         // Allowed by both global and profile allow list
-        auditor.IsIpAllowed("SecureZone", AllowedIpv4).ShouldBeTrue();
+        auditor.IsIpAllowed("SecureZone", IPAddress.Parse("10.0.0.1")).ShouldBeTrue();
 
         // Denied by global deny list
-        auditor.IsIpAllowed("SecureZone", DeniedIpv4).ShouldBeFalse();
+        auditor.IsIpAllowed("SecureZone", IPAddress.Parse("203.0.113.42")).ShouldBeFalse();
 
         // Denied by profile deny list
         auditor.IsIpAllowed("SecureZone", IPAddress.Parse("198.51.100.5")).ShouldBeFalse();
 
         // Not in allow lists, so denied
-        auditor.IsIpAllowed("SecureZone", UnlistedIpv4).ShouldBeTrue();
+        auditor.IsIpAllowed("SecureZone", IPAddress.Parse("192.168.99.99")).ShouldBeTrue();
     }
 }
